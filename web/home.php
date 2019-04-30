1f743230c1c3e5ec807c71cb9d7a6c62064c2ef1
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-04-30 15:45:05 -0600

    added home and assignment pages

---
+++ web/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<title>Home</title>
+</head>
+<body>
+	<header>
+		<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="assignments.html">Assignments</a></li>
+		</ul>
+	</header>
+	<main>
+		<br><br><br>
+		<h1 id="intro">Introduction</h1>
+		<div id="about"> 
+			<h2>About Me</h2>
+			<p>Something that really interesting about me is get bored really quick with what I am doing so I continue to pick new things up. For this reason I really like to do new things all the time. I am also really into super heroes espically Marvel. My favorite super hero is Spider-man because we have similar personalities and is totally awesome. Something that I really to do on my free time learning about the new mobile technologies like smartphones and tablets. I just find it really interesting learn about them. For those that what phone I perfer and that is iPhones. I also love doing random things all the time and really grateful that my wife allows me to act weird all the time. Also super excited because my wife is pregnant and I can't wait to have with my kid.</p>
+		</div>
+		<img src="spiderman.jpg" alt="Spider-man" id="spider" height="300" width="500">
+
+		<div id="hobby">
+			<h2>Hobbies</h2>
+		<p>Something of things that I do know is I have a lot of rubik's cubes. I have total of 8 rubik's cube add made up of varity of different ones. I also really like to play games rather it is boards or even videos games. Currently I am play the new spiderman game and it is totally awesome. I also really like to watching movies and tv shows.</p>
+		</div>
+
+		<img src="cube.jpg" alt="Rubik's cubes" width="500" height="300"> 
+
+	</main>
+	<footer>
+		<span>&copy </span></script><?php echo date("Y"); ?>
+	</footer>
+</body>
+</html>
