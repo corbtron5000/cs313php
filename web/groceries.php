9548e42dd5fbe9961fb8f6961a8e6836719ee394
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-22 18:31:18 -0600

    adding header

--- web/groceries.php
+++ web/groceries.php
@@ -1,9 +1,23 @@
+<?php
+	require("dbConnect.php");
+	$db = get_db();
+	
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="homeMeal.css">
 </head>
 <body>
+	<header>
+		<h1 id="title">Meal Shopper</h1>
+		<ul>
+			<li><a href="homeMeal.php" class="link">Home</a></li>
+			<li><a href="meal.php" class="link">Add Meal</a></li>
+			<li><a href="groceries" class="link">Grocery List</a></li>
+		</ul>
+	</header>
 
 </body>
 </html>
