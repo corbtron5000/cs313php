5201c24c4c9d0bf6c9567fa822ef188918f87125
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-22 18:33:29 -0600

    fixing link groceries

--- web/groceries.php
+++ web/groceries.php
@@ -15,7 +15,7 @@
 		<ul>
 			<li><a href="homeMeal.php" class="link">Home</a></li>
 			<li><a href="meal.php" class="link">Add Meal</a></li>
-			<li><a href="groceries" class="link">Grocery List</a></li>
+			<li><a href="groceries.php" class="link">Grocery List</a></li>
 		</ul>
 	</header>
 
