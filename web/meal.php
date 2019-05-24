050fa261539c8658a6336f0f02f881280454c882
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-23 20:42:08 -0600

    jqeary add

--- web/meal.php
+++ web/meal.php
@@ -8,11 +8,14 @@
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="homeMeal.css">
-</head>
-<body>
-	<header>
-		<h1 id="title">Meal Shopper</h1>
-		<ul>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+	<script>
+		$(function() {
+			$('#add').click(function() {
+				$('dv.ingredDiv:first').clone().insertAfter(this).find('input').val('')
+			});
+		});
+	</script>
 			<li><a href="prove5.php" class="link">Home</a></li>
 			<li><a href="meal.php" class="link">Add Meal</a></li>
 			<li><a href="groceries.php" class="link">Grocery List</a></li>
@@ -20,7 +23,33 @@
 	</header>
 	<main>
 		<br><br><br><br><br><br><br>
-		<h1>Add a Meal<h1>
+		<h1>Add a Meal</h1>
+		<form>
+
+			<label>Meal Name</label><br>
+			<input type="text" name="name" placeholder="Enter a Meal Name">
+
+			<label>Description</label><br>
+			<textarea name="description" placeholder="Enter a description"></textarea>
+
+			<label>Directions</label><br>
+			<textarea name="Directions" placeholder="Enter the directions"></textarea>
+
+			<label>Serving Size</label><br>
+			<input type="text" name="size" placeholder="Enter a integer">
+
+			<div>
+				<div class="ingredDiv">
+					<label>Ingredients</label><br>
+					<input type="text" id="ingre" name="ingred[]" placeholder="Enter the Ingredients name">
+					<input type="text" id="quantity" name="quantity[]" placeholder="Enter decimal number amount: 1.5">
+					<input type="text" id="measure" name="measure[]" pattern="Enter measurement: Cup">
+				</div>
+			</div>
+			<button id="add">Add New Ingredient</button>
+
+		</form>
+
 
 	</main>
 
