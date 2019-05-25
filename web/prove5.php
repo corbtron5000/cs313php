b60cbf530a97b243640c77522dce6f29a17005f0
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-25 11:28:14 -0600

    fixing php error

--- web/prove5.php
+++ web/prove5.php
@@ -35,7 +35,7 @@
 				$id = $row['meals_id'];
 				$desc = $row['description'];
 
-				echo "<a href="meal.php" class="meals"><div id=div>";
+				echo "<a href=meal.php class=meals><div id=div>";
 				echo "<h2 id=name>$name</h2>";
 				echo "<h3 id=description>Description</h3>";
 				echo "<p id=desc>$desc</p>";
