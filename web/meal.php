9afae921cfb65c308e7088dbf0ab7b773fb561c5
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-25 10:57:52 -0600

    more css

--- web/meal.php
+++ web/meal.php
@@ -31,24 +31,24 @@
 		<h1>Add a Meal</h1>
 
 		<div id="from">
-			<label>Meal Name</label><br>
-			<input type="text" name="name" placeholder="Enter a Meal Name"><br>
+			<label>Meal Name</label>
+			<input type="text" name="name" placeholder="Enter a Meal Name"><br><br>
 
 			<label>Description</label><br>
-			<textarea name="description" placeholder="Enter a description" rows="5" cols="100"></textarea><br>
+			<textarea name="description" placeholder="Enter a description" rows="5" cols="100"></textarea><br><br>
 
 			<label>Directions</label><br>
-			<textarea name="Directions" placeholder="Enter the directions" rows="5" cols="100"></textarea><br>
+			<textarea name="Directions" placeholder="Enter the directions" rows="5" cols="100"></textarea><br><br>
 
 			<label>Serving Size</label><br>
-			<input type="text" name="size" placeholder="Enter a integer">
+			<input type="text" name="size" placeholder="Enter a integer"><br><br>
 
 			<div>
-				<label>Ingredients</label><br>
+				<label>Ingredients</label><br><br>
 				<div class="div">
 					<label>Enter Ingredient </label>
 					<input type="text" id="ingre" name="ingred[]" placeholder="Enter the Ingredients name"><br>
-					<label>Enter Quantity measurementa as Number </label>
+					<label>Enter Quantity </label>
 					<input type="text" id="quantity" name="quantity[]" placeholder="1.5"><br>
 					<label>Enter Measurement Type </label>
 					<input type="text" id="measure" name="measure[]" placeholder ="Cup">
