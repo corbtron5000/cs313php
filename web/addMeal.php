5a237094ccbe269f3422dff5fa2e15cc92c4527f
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-28 18:35:16 -0600

    adding addMeal

--- web/addMeal.php
+++ web/addMeal.php
@@ -24,7 +24,7 @@
 
 		<?php 
 
-			$statement = $db->prepare("SELECT meals_id, name, serving_size, description FROM meals");
+			$statement = $db->prepare("SELECT meals_id, name, serving_size, description, directions FROM meals");
 			$statement->execute();
 
 			echo "<div id=parent>";
@@ -34,13 +34,11 @@
 				$serving = $row['serving_size'];
 				$id = $row['meals_id'];
 				$desc = $row['description'];
+				$direction = $row['directions'];
 
-				echo "<a href=addMeal.php class=meals><div id=div>";
-				echo "<h2 id=name>$name</h2>";
-				echo "<h3 id=description>Description</h3>";
-				echo "<p id=desc>$desc</p>";
-				echo "<label id=size>Serving Size: $serving</label>";
-				echo "</div></a>";
+
+				echo "<label>Meal Name </label>";
+				echo "<input type='text' name='name' placeholder='Enter a Meal Name' value='$name'><br><br>"
 			}
 			echo "</div>";
 		?>
---
+++ web/meal.sql
@@ -0,0 +1,7 @@
+insert into meals(directions) values ('1 Mix powdered sugar and butter with spoon or electric mixer on low speed. Stir in vanilla and 1 tbsp of the milk. Add more milk gradually if desired for consistency. 
+
+	2 If adding chocolate, melt the chocolate in a glass bowl in the microwave, then fold into the frosting.') where meals.meals_id = 1;
+
+update meals set directions = '1 Mix powdered sugar and butter with spoon or electric mixer on low speed. Stir in vanilla and 1 tbsp of the milk. Add more milk gradually if desired for consistency. 
+
+	2 If adding chocolate, melt the chocolate in a glass bowl in the microwave, then fold into the frosting.' where meals_id = 1;
