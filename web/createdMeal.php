8439df484b894c4827faf3abfa5f761f6e7842ec
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-05-31 20:20:09 -0600

    hoping this works

--- web/createdMeal.php
+++ web/createdMeal.php
@@ -54,7 +54,7 @@
 
 				echo("Did I get here: $ingredient<br>");
 
-				$statement = $db->prepare('SELECT name, ingredients_id FROM ingredients where name = "$ingred"');
+				$statement = $db->prepare('SELECT name, ingredients_id FROM ingredients where name = $ingredient');
 				$statement->execute();
 
 				echo "I am here";
